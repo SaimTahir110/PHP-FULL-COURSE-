a238a2c0c5b2c56808c37986e82a94f89e4e176f
Saim Tahir <saimtahirs786@gmail.com>
2024-02-04 19:59:44 +0500

    new addition

--- If Statement.php
+++ If Statement.php
@@ -1,3 +1,27 @@
 if condition alwas have two answers "true" or "fals"
 If condition is "true" statment will be printed.
 if condition is "fals"statment will not printed its "out"
+
+if (condition){
+
+}
+
+FOR EXMPL:
+
+$X =10;
+if ($X > 5);
+echo "value of x is greater than five";
+
+
+<?php
+
+$A = 10;
+$B = 6;
+
+if ($A > $B) {
+
+    echo "Value of A is greater than Value of B";
+}
+
+?>
+
