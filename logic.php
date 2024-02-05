b92580d8d5b18ee09bc5e16930590e7aa6bf4eb4
Saim Tahir <saimtahirs786@gmail.com>
2024-02-05 09:52:45 +0500

    logic oprator in php

---
+++ logic.php
@@ -0,0 +1,22 @@
+
+<?php
+
+/*Logics oprator use in PHP for More the one condition.
+iF Both Condition Is "True" statment will be printe.
+If both "fals"statment will be out.
+
+AND  &&  if (condition1 && condition2){} if both condition "True"statment printe.
+OR   ||  if (condition1 || condition2){} if one condition "True" and other "fals" then statment printe. if both condition "True"statment also printe.
+NOT  !   
+AND  and
+OR   or
+XOR  XOR */
+
+$age = 20;
+
+if ($age >= 18 && $age <= 21) {
+
+    echo"you can login now";
+}
+
+?>
