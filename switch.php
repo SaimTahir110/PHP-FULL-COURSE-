0cb368077f40c9f604f048033d0d305b23932cd0
Saim Tahir <saimtahirs786@gmail.com>
2024-02-06 19:58:02 +0500

    switch statment by use break function

---
+++ switch.php
@@ -0,0 +1,48 @@
+
+<?php
+
+$WEEKDAY = 3;
+
+switch($WEEKDAY){
+
+    case 1:
+    echo "today is monday";
+    break;
+
+    case 2:
+        echo "today is tuesday";
+        break;
+
+        case 3:
+            echo "today is wednesday";
+            break;
+
+
+            case 4:
+                echo "today is thursday";
+                break;
+
+                case 5:
+                    echo "today is friday";
+                    break;
+
+
+                    case 6:
+                        echo "today is saturday";
+                        break;
+
+
+                        case 7:
+                            echo "today is sunday";
+                            break;
+
+
+                            case 8:
+                                echo "enter valid week day number";
+                                break;
+
+
+}
+
+
+?>
