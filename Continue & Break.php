904df260bc905aaf909d8f1c8d59203a7c8e9eac
Saim Tahir <saimtahirs786@gmail.com>
2024-02-11 10:21:46 +0500

    new php

---
+++ Continue & Break.php
@@ -0,0 +1,18 @@
+
+<?php
+
+for($a = 1; $a <= 10; $a++){
+
+    if ($a == 4){
+
+  echo "No" . $a . "<br>";      
+  
+  break;
+
+    }
+
+    echo "Number" . $a . "<br>";      
+
+}
+
+?>
---
+++ Do While.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$a = 1;
+
+do{
+
+echo "my name is saim tahir<br >";
+$a++;
+
+}while ($a <= 10);
+
+?>
