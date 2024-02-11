39ca332a1a67e081e414dfb7ddd6240992332234
Saim Tahir <saimtahirs786@gmail.com>
2024-02-11 10:41:38 +0500

    goto or jump other code

---
+++ Goto Statement.php
@@ -0,0 +1,19 @@
+
+<?php
+
+echo "My name is saim tahir<br>";
+echo "My name is saim tahir<br>";
+echo "My name is saim tahir<br>";
+echo "My name is saim tahir<br>";
+goto abc;
+echo "My name is saim tahir<br>";
+echo "My name is saim tahir<br>";
+echo "My name is saim tahir<br>";
+
+
+
+abc:
+echo "I am jumping to the label 'abc'";
+
+
+?>
