2873041a45fcef58cd9de4322975d27239c71498
Saim Tahir <saimtahirs786@gmail.com>
2024-02-11 18:20:15 +0500

    array starting

--- PHP Array.php
+++ PHP Array.php
@@ -1,8 +1,14 @@
 
 <?php
 
+$colors = array("blue","green","yellow","orang","black","withe","gray") ; 
+/*echo "$colors[0]<br>"; // blue
+echo "$colors[1]<br>";
+echo $colors [2];*/
 
-
-
+//Also use print_r () function to display the content of an Array.
+echo "pre ";
+print_r($colors);
+echo "/pre" ;
 
 ?>
